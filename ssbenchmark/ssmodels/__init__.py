c032c5090fd2b59a95e9fb821fb3168243dedd96
Paula Torren Peraire <ptorrenp@gmail.com>
2024-10-07 13:42:36 +0200

    Introduce optional dependencies

--- ssbenchmark/ssmodels/__init__.py
+++ ssbenchmark/ssmodels/__init__.py
@@ -1,6 +1,5 @@
 from .base_model import Registries
 
-from .model_chemformer import model_chemformer
-from .model_localretro import model_localretro
-from .model_mhn_react import model_mhn_react
-
+# from .model_chemformer import model_chemformer
+# from .model_localretro import model_localretro
+# from .model_mhn_react import model_mhn_react
